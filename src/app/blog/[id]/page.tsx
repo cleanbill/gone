b38e44826ad712fb7df23938057f2be6e1066781
Mick Carter <cick.marter@gmail.com>
2024-09-28 15:54:21 +0100

    Added posts

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,12 +1,11 @@
-//import allPosts from '@/data/allclean.json';
+import allPosts from '@/data/allclean.json';
 import Posts from "@/components/blog";
 
 export default function Home({ params }: { params: { id: string } }) {
 
   return (
     <main className="flex min-h-screen flex-col items-center max-w-fit">
-      {params.id}
-      {/* <Posts allPosts={allPosts}></Posts> */}
+      <Posts allPosts={allPosts}></Posts>
     </main >
   );
 }
